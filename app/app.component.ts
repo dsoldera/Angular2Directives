6906d7670317ad3473c9bc76b4f87cd78ebc142a
dsoldera <daniellesoldera@gmail.com>
2016-12-03 16:08:05 -0200

    first commit

---
+++ app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    moduleId: module.id,
+    selector: 'app',
+    template: `
+              <h1>My Angular 2 Directives</h1>
+              <router-outlet></router-outlet>`
+})
+
+export class AppComponent {}
